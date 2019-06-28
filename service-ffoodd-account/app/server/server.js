1656a3a24b164015ef626e3c9ea2dcff86a01922
Quang Dat <quangdat2000.pham@gmail.com>
2019-06-28 16:07:49 +0530

    update registerServiceProtos func, create register.client

--- service-ffoodd-account/app/server/server.js
+++ service-ffoodd-account/app/server/server.js
@@ -17,15 +17,6 @@ const packageDefinitionOptions =  {
 const accountPackageDefinition = protoLoader.loadSync(ACCOUNT_PROTO_PATH, packageDefinitionOptions);
 const registerPackageDefinition = protoLoader.loadSync(REGISTER_PROTO_PATH, packageDefinitionOptions);
 
-const registerServiceProtos = async (call, callback) => {
-  const accountProtoFile = {
-    name: 'account',
-    content: fs.readFileSync(ACCOUNT_PROTO_PATH, { encoding: 'utf-8' })
-  };
-
-  callback(null, { protoFiles: [ accountProtoFile ] });
-}
-
 const start = ({ logger, rootRoute }) => async () => {
   process.on('uncaughtException', err => {  
     logger.error('Unhandled Exception', err);
@@ -34,6 +25,18 @@ const start = ({ logger, rootRoute }) => async () => {
   process.on('uncaughtRejection', (err, promise) => {
     logger.error('Unhandled Rejection', err);
   });
+
+  const registerServiceProtos = async (call, callback) => {
+    logger.info('Sending proto files -----> client', call);
+
+    const accountProtoFile = {
+      name: 'account',
+      content: fs.readFileSync(ACCOUNT_PROTO_PATH, { encoding: 'utf-8' })
+    };
+
+    callback(null, { protoFiles: [ accountProtoFile ] });
+  }
+
 
   const AccountProto = grpc.loadPackageDefinition(accountPackageDefinition).account;
   const RegisterProto = grpc.loadPackageDefinition(registerPackageDefinition).register;

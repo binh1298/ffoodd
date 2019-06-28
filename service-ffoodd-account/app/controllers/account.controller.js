fe7b96747701ca23ec17a403aac4c3bac7c4866b
Quang Dat <quangdat2000.pham@gmail.com>
2019-06-28 23:40:31 +0530

    mistake syntax error account.proto, return updateEmailById account.controller

--- service-ffoodd-account/app/controllers/account.controller.js
+++ service-ffoodd-account/app/controllers/account.controller.js
@@ -115,6 +115,7 @@ module.exports = ({ accountRepository: Account }) => {
     verifyEmail,
     findByUsername,
     resetPassword,
-    updatePasswordById
+    updatePasswordById,
+    updateEmailById
   }
 }

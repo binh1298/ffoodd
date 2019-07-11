bbe2eaed33c9aef4533c938545f0b631b11f969f
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-11 11:53:13 +0530

    update email.js lib, sendWithTemplate

--- service-ffoodd-notification/app/libs/email.js
+++ service-ffoodd-notification/app/libs/email.js
@@ -4,7 +4,10 @@ const { to } = require('await-to-js');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = ({ logger }) => {
-  const sendEmail = async ({ recipients, sender, subject, text, html, cc, bcc, replyTo }) => {
+
+  const send = async options => {
+    const { recipients, sender, subject, text, html, cc, bcc, replyTo } = options;
+
     const [ err ] = await to(sgMail.send({
       to: recipients,
       cc,
@@ -20,7 +23,27 @@ module.exports = ({ logger }) => {
     logger.info(`Email with subject *${subject}* has been sent to`, recipients);
   }
 
+  const sendWithTemplate = async options => {
+    const { recipients, sender, subject, text, html, cc, bcc, replyTo, templateId, dynamicTemplateData } = options;
+    
+    const [ err ] = await to(sgMail.send({
+      to: recipients,
+      cc,
+      bcc,
+      from: sender,
+      replyTo,
+      subject,
+      text,
+      html,
+      templateId,
+      dynamic_template_data: dynamicTemplateData
+    }));
+
+    logger.info(`Email with subject *${subject}* has been sent to`, recipients);
+  }
+
   return {
-    sendEmail
+    send,
+    sendWithTemplate
   };
 }

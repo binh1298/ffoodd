c91f5261bd5c120d83022c73c48811b7b2c630e4
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-13 19:15:37 +0530

    call meal-service to save category

--- service-ffoodd-pantry/app/pantries/themealbd/find-all-categories.themealdb.pantry.js
+++ service-ffoodd-pantry/app/pantries/themealbd/find-all-categories.themealdb.pantry.js
@@ -3,23 +3,31 @@ const { to } = require('await-to-js');
 
 const THEMEALDB_API = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 
-module.exports = ({ logger }) => async () => {
-  const [ err, res ] = await to(axios.get(THEMEALDB_API));
-  if (err) throw err;
+module.exports = ({ logger, mealGRPCClientService }) => {
+  const Category = mealGRPCClientService.category;
 
-  const { categories } = res.data;
-  if (!categories) return null; 
+  const findAllCategories = async () => {
+    const [ err, res ] = await to(axios.get(THEMEALDB_API));
+    if (err) throw err;
 
-  for (category of categories) {
-    const { idCategory, strCategory, strCategoryThumb, strCategoryDescription } = category;
-    const ffooddCategory = {
-      name: strCategory,
-      description: strCategoryDescription,
-      imageUrl: strCategoryThumb
-    };
+    const { categories } = res.data;
+    if (!categories) return null; 
 
-    logger.info(`Creating new ffoodd category: ${ffooddCategory.name}`);
+    for (category of categories) {
+      const { idCategory, strCategory, strCategoryThumb, strCategoryDescription } = category;
+      const ffooddCategory = {
+        name: strCategory,
+        description: strCategoryDescription,
+        imageUrl: strCategoryThumb
+      };
+
+      Category.create({ category: ffooddCategory });
+
+      logger.info(`Creating new ffoodd category: ${ffooddCategory.name}`);
+    }
+
+    return categories;
   }
 
-  return categories;
+  return findAllCategories;
 }

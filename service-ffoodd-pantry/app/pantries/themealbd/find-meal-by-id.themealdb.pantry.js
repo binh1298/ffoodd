4c09c1f7cbc0d10d615eb3fc4c9a7ded06097aad
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-13 19:18:13 +0530

    call meal-service to save meal

--- service-ffoodd-pantry/app/pantries/themealbd/find-meal-by-id.themealdb.pantry.js
+++ service-ffoodd-pantry/app/pantries/themealbd/find-meal-by-id.themealdb.pantry.js
@@ -3,53 +3,61 @@ const { to } = require('await-to-js');
 
 const THEMEALDB_API = 'https://www.themealdb.com/api/json/v1/1/lookup.php';
 
-module.exports = ({ logger }) => async ({ id }) => {
-  const [ err, res ] = await to(axios.get(THEMEALDB_API, {
-    params: {
-      i: id
-    }
-  }));
-  if (err) throw err;
+module.exports = ({ logger, mealGRPCClientService }) => {
+  const Meal = mealGRPCClientService.meal;
 
-  const { meals } = res.data;
-  if (!meals) return null;
+  const findMealById = async ({ id }) => {
+    const [ err, res ] = await to(axios.get(THEMEALDB_API, {
+      params: {
+        i: id
+      }
+    }));
+    if (err) throw err;
 
-  const meal = meals[0];
-  const {
-    idMeal,
-    strMeal,
-    strDrinkAlternate,
-    strCategory,
-    strArea,
-    strInstructions,
-    strMealThumb,
-    strTags,
-    strYoutube,
-    strSource,
-    dateModified,
-  } = meal;
+    const { meals } = res.data;
+    if (!meals) return null;
 
-  const tags = strTags.split(',');
+    const meal = meals[0];
+    const {
+      idMeal,
+      strMeal,
+      strDrinkAlternate,
+      strCategory,
+      strArea,
+      strInstructions,
+      strMealThumb,
+      strTags,
+      strYoutube,
+      strSource,
+      dateModified,
+    } = meal;
 
-  const ingredients = Array.from({ length: 20 }).map((v, i) => ({
-    ingredient: meal[`strIngredient${i + 1}`],
-    measure: meal[`strMeasure${i + 1}`]
-  }));
+    const tags = strTags.split(',');
 
-  const ffooddMeal = {
-    themealdb_id: idMeal,
-    name: strMeal,
-    origin: strArea,
-    images: [
-      strMealThumb
-    ],
-    recipe: {
-      instruction: strInstructions,
-      ingredients: ingredients
-    }
-  };
+    const ingredients = Array.from({ length: 20 }).map((v, i) => ({
+      ingredient: meal[`strIngredient${i + 1}`],
+      measure: meal[`strMeasure${i + 1}`]
+    }));
 
-  logger.info(`Creating new ffoodd meal: ${ffooddMeal.name}`);
+    const ffooddMeal = {
+      themealdb_id: idMeal,
+      name: strMeal,
+      origin: strArea,
+      images: [
+        strMealThumb
+      ],
+      recipe: {
+        instruction: strInstructions,
+        ingredients: ingredients
+      }
+    };
 
-  return meal;
+    Meal.create({ meal: ffooddMeal });
+
+    logger.info(`Creating new ffoodd meal: ${ffooddMeal.name}`);
+
+    return meal;
+  }
+
+  return findMealById;
 }

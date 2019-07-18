f0263c51c7d4fe95f9d0e1c20f30dd502466667f
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-18 15:44:58 +0530

    consume account event

--- service-ffoodd-notification/app/amqp-consumers/account.consumer.js
+++ service-ffoodd-notification/app/amqp-consumers/account.consumer.js
@@ -1,17 +1,47 @@
 const { to } = require('await-to-js');
 
-module.exports = ({ amqp }) => {
+module.exports = ({ amqp, accountRepository: Account }) => {
   
   const consume = async () => {
-    consumeCreateAccount();
-  }
-
-  const consumeCreateAccount = async () => {
-    const [ err, msg ] = await to(amqp.consume({ queue: 'account' }));
-    if (err) throw err;
+    const [ err0, msg ] = await to(amqp.consume({ queue: 'account' }));
+    if (err0) throw err0;
 
     const { event, data } = msg;
+    let consumeEvent;
+
+    switch (event) {
+      case 'CREATE_ACCOUNT':
+        consumeEvent = consumeCreateAccount;
+        break;
+      case 'UPDATE_EMAIL':
+        consumeEvent = consumeUpdateEmail;
+        break;
+      case 'REMOVE_ACCOUNT':
+        consumeEvent = consumeRemoveAccount;
+        break;
+    }
+
+    const [ err1 ] = await to(consumeEvent(msg));
+    if (err1) throw err1;
   }
 
-  return consume;
+  const consumeCreateAccount = async msg => {
+    const { _id, email } = msg.data;
+    const [ err ] = await to(Account.create({ account_id: _id, email }));
+    if (err) throw err;
+  }
+
+  const consumeUpdateEmail = async msg => {
+    const { _id, email } = msg.data;
+    const [ err ] = await to(Account.udpate({ account_id: _id, email }));
+    if (err) throw err;
+  }
+
+  const consumeRemoveAccount = async msg => {
+    const { _id } = msg.data;
+    const [ err ] = await to(Account.remove({ account_id: _id }));
+    if (err) throw err;
+  }
+
+  return { consume };
 }

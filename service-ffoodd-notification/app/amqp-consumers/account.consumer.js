1245b59a0bb99e29e1eeaa3fecf8881569374e54
Quang Dat <quangdat2000.pham@gmail.com>
2019-07-18 15:53:36 +0530

    refactor amqp concume fn

--- service-ffoodd-notification/app/amqp-consumers/account.consumer.js
+++ service-ffoodd-notification/app/amqp-consumers/account.consumer.js
@@ -3,26 +3,26 @@ const { to } = require('await-to-js');
 module.exports = ({ amqp, accountRepository: Account }) => {
   
   const consume = async () => {
-    const [ err0, msg ] = await to(amqp.consume({ queue: 'account' }));
-    if (err0) throw err0;
+    amqp.consume({ queue: 'account' }, () => {
+      const { event, data } = msg;
+      let consumeEvent;
 
-    const { event, data } = msg;
-    let consumeEvent;
+      switch (event) {
+        case 'CREATE_ACCOUNT':
+          consumeEvent = consumeCreateAccount;
+          break;
+        case 'UPDATE_EMAIL':
+          consumeEvent = consumeUpdateEmail;
+          break;
+        case 'REMOVE_ACCOUNT':
+          consumeEvent = consumeRemoveAccount;
+          break;
+        default:
+          return;
+      }
 
-    switch (event) {
-      case 'CREATE_ACCOUNT':
-        consumeEvent = consumeCreateAccount;
-        break;
-      case 'UPDATE_EMAIL':
-        consumeEvent = consumeUpdateEmail;
-        break;
-      case 'REMOVE_ACCOUNT':
-        consumeEvent = consumeRemoveAccount;
-        break;
-    }
-
-    const [ err1 ] = await to(consumeEvent(msg));
-    if (err1) throw err1;
+      consumeEvent(msg);
+    })
   }
 
   const consumeCreateAccount = async msg => {
